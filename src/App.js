dbd4dd72a49bb31f77923eb98afc5cae8675f574
Gonzalez32 <3232rose@gmail.com>
2021-05-30 12:29:01 -0500

    Import into App.ja

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
+
 
 function App() {
+  const title = "Welcome to your new blog";
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        {/* <h1>App Component</h1> */}
+        {/* <h1>{ title }</h1> */}
+        <Home />
       </div>
     </div>
   );
